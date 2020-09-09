b95876111c5eb83cd03f58ca7eef3b7c51641ef7
Fernando Almeida (do quarto) <fernandodealmeida@hotmail.com.br>
2020-09-09 13:17:02 -0300

    código verificado no programa Arduino

--- passa-repassa.ino
+++ passa-repassa.ino
@@ -1,18 +1,20 @@
 typedef struct{
-    const int pino_botao;
-    const int pino_led;
+    int pino_botao;
+    int pino_led;
     boolean jogador_en;
-} player;
+} jog_participante;
+
+const int botao_rs = 1;
+bool flag_pressed_btn = false;
+int timer = 0;
+int rs_clk = 0;
+jog_participante az     = { 1, 1, 1};
+jog_participante vm     = { 1, 1, 1};
+jog_participante am     = { 1, 1, 1};
+jog_participante vd     = { 1, 1, 1};
 
 
 void setup(){
-    player az     = { 1, 1, 1};
-    player vm     = { 1, 1, 1};
-    player am     = { 1, 1, 1};
-    player vd     = { 1, 1, 1};
-    const int botao_rs = 1;
-    bool flag_pressed_btn = false;
-    int timer = 0;
 
     participantes_init(az);
     participantes_init(vm);
@@ -39,7 +41,10 @@ void loop(){
         }
     }else{
         venceJogo(timer);
-        if(digitalRead(botao_rs) == LOW){};
+        if(digitalRead(botao_rs) == LOW){
+            if (millis() - rs_clk > 3000) selecionaParticipantes();
+            else flag_pressed_btn = 0;
+        } else rs_clk = millis();
     }               
 // comparar com o que ja tem escrito
 // TODO 
@@ -50,13 +55,23 @@ void loop(){
 
 void venceJogo(int tempo_jogo){
     int indice = 0;
-    if (millis() - tempo_jogo < 400) indice = 0;
+    int periodo_comutacoes[] = {100, 50, 20, 0};
+    jog_participante vencedor = {0, 0, 0}; 
+    if(digitalRead(az.pino_led) == HIGH) vencedor = az;
+    if(digitalRead(vm.pino_led) == HIGH) vencedor = vm;
+    if(digitalRead(am.pino_led) == HIGH) vencedor = am;
+    if(digitalRead(vd.pino_led) == HIGH) vencedor = vd;
+    if(millis() - tempo_jogo < 400) indice = 0;
     else if (millis() - tempo_jogo < 600) indice = 1;
     else if (millis() - tempo_jogo < 800) indice = 2;
     else indice = 3;
+    if((millis() - tempo_jogo) % periodo_comutacoes[indice]){
+      digitalWrite(vencedor.pino_led, !digitalRead(vencedor.pino_led));
+      if(indice == 3) digitalWrite(vencedor.pino_led, HIGH);
+    }
         
 }
-void participantes_init(player jogador){
+void participantes_init(jog_participante jogador){
     pinMode(jogador.pino_led, OUTPUT);
     pinMode(jogador.pino_botao, INPUT);
     jogador.jogador_en = 1;
