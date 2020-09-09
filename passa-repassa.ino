353af8151a771b9101a9c710ae97d263103bd625
escritorio <escritorio>
2020-09-09 12:06:28 -0300

    mexi no loop()

--- passa-repassa.ino
+++ passa-repassa.ino
@@ -6,21 +6,41 @@ typedef struct{
 
 
 void setup(){
-    player azul     = { 1, 1, 1};
-    player vermelho = { 1, 1, 1};
-    player amarelo  = { 1, 1, 1};
-    player verde    = { 1, 1, 1};
+    player az     = { 1, 1, 1};
+    player vm     = { 1, 1, 1};
+    player am     = { 1, 1, 1};
+    player vd     = { 1, 1, 1};
     const int botao_rs = 1;
-    bool flat_pressed_btn = false;
+    bool flag_pressed_btn = false;
+    int timer = 0;
 
-    participantes_init(azul);
-    participantes_init(vermelho);
-    participantes_init(verde);
-    participantes_init(amarelo);
+    participantes_init(az);
+    participantes_init(vm);
+    participantes_init(vd);
+    participantes_init(am);
     pinMode(botao_rs, INPUT);
 }
 
 void loop(){
+    if (!flag_pressed_btn){
+        timer = millis();
+        if (digitalRead(az.pino_botao) == LOW){ 
+            flag_pressed_btn = 1;
+            digitalWrite(az.pino_led, HIGH);
+        }else if (digitalRead(vm.pino_botao) == LOW){ 
+            flag_pressed_btn = 1;
+            digitalWrite(vm.pino_led, HIGH);
+        }else if (digitalRead(vd.pino_botao) == LOW){ 
+            flag_pressed_btn = 1;
+            digitalWrite(vd.pino_led, HIGH);
+        }else if (digitalRead(am.pino_botao) == LOW){ 
+            flag_pressed_btn = 1;
+            digitalWrite(am.pino_led, HIGH);
+        }
+    }else{
+        venceJogo(timer);
+        if(digitalRead(botao_rs) == LOW){};
+    }               
 // comparar com o que ja tem escrito
 // TODO 
 // [ ] piscar leds
@@ -28,11 +48,12 @@ void loop(){
 // [ ] 
 }
 
-void venceJogo(player vencedor){
-    int tempo_ciclo = millis();
+void venceJogo(int tempo_jogo){
     int indice = 0;
-    if (millis() - tempo_ciclo < 100) indice = 0;
-    int intervalos[] = {200, 200, 200, 100, 100, 100, 50, 50};
+    if (millis() - tempo_jogo < 400) indice = 0;
+    else if (millis() - tempo_jogo < 600) indice = 1;
+    else if (millis() - tempo_jogo < 800) indice = 2;
+    else indice = 3;
         
 }
 void participantes_init(player jogador){
@@ -41,17 +62,17 @@ void participantes_init(player jogador){
     jogador.jogador_en = 1;
 }
 void selecionaParticipantes(){
-    digitalWrite(azul.pino_led, !digitalRead(azul.pino_botao));
-    digitalWrite(vermelho.pino_led, !digitalRead(vermelho.pino_botao));
-    digitalWrite(verde.pino_led, !digitalRead(verde.pino_botao));
-    digitalWrite(amarelo.pino_led, !digitalRead(amarelo.pino_botao));
+    digitalWrite(az.pino_led, !digitalRead(az.pino_botao));
+    digitalWrite(vm.pino_led, !digitalRead(vm.pino_botao));
+    digitalWrite(vd.pino_led, !digitalRead(vd.pino_botao));
+    digitalWrite(am.pino_led, !digitalRead(am.pino_botao));
 
-    if (digitalRead(azul.pino_botao) == LOW) azul.jogador_en = true;
-    else azul.jogador_en = false;
-    if (digitalRead(vermelho.pino_botao) == LOW) vermelho.jogador_en = true; 
-    else vermelho.jogador_en = false;
-    if (digitalRead(amarelo.pino_botao) == LOW) amarelo.jogador_en = true;
-    else amarelo.jogador_en = false;
-    if (digitalRead(verde.pino_botao) == LOW) verde.jogador_en = true;
-    else verde.jogador_en = false;
+    if (digitalRead(az.pino_botao) == LOW) az.jogador_en = true;
+    else az.jogador_en = false;
+    if (digitalRead(vm.pino_botao) == LOW) vm.jogador_en = true; 
+    else vm.jogador_en = false;
+    if (digitalRead(am.pino_botao) == LOW) am.jogador_en = true;
+    else am.jogador_en = false;
+    if (digitalRead(vd.pino_botao) == LOW) vd.jogador_en = true;
+    else vd.jogador_en = false;
 }
